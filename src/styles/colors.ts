ea82e41a7c3f7ab5cdf5e6f39cbc7e2a6e729448
Henry Fellerhoff <henryfellerhoff@gmail.com>
2021-11-02 14:30:25 -0500

    add app colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const Colors: Record<string, any> = {
+  primary: '#E1944C',
+  text: '#606060',
+  lines: {
+    red: '#C60C30',
+    blue: '#02A1DE',
+    purple: '#522398',
+    yellow: '#FFD234',
+    green: '#069B49',
+    orange: '#EF4925',
+    brown: '#63361C',
+    pink: '#E27FA7',
+  },
+};
