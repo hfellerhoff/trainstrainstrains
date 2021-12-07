62de15e97c66b7dcdcf75dccd5c5cbb342af172f
Henry Fellerhoff <henryfellerhoff@gmail.com>
2021-12-07 13:30:47 -0600

    fix react no-op errors

--- src/hooks/useFavorites.tsx
+++ src/hooks/useFavorites.tsx
@@ -1,13 +1,15 @@
 import {useEffect, useState} from 'react';
 import {useRecoilState} from 'recoil';
-import {Favorites, favoritesAtom} from '../recoil/atoms';
+import {
+  Favorites,
+  favoritesAtom,
+  FAVORITES_LOCAL_STORAGE_KEY,
+} from '../recoil/atoms';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface FavoriteOptions {
   shouldSyncWithLocalStorage?: boolean;
 }
-
-const FAVORITES_LOCAL_STORAGE_KEY = '@favorites';
 
 const useFavorites = (
   {shouldSyncWithLocalStorage = false}: FavoriteOptions = {
@@ -38,51 +40,23 @@ const useFavorites = (
     }
   }, []);
 
-  const setLocalStorageValues = async (values: Favorites) => {
-    try {
-      const favoritesToSave = Object.entries(values).reduce(
-        (acc, [map_id, isFavorited]) => {
-          if (isFavorited) {
-            acc[map_id] = true;
-          }
-          return acc;
-        },
-        {} as Favorites,
-      );
-      await AsyncStorage.setItem(
-        FAVORITES_LOCAL_STORAGE_KEY,
-        JSON.stringify(favoritesToSave),
-      );
-    } catch (e) {
-      console.error(
-        `Error setting LocalStorage value ${FAVORITES_LOCAL_STORAGE_KEY}.`,
-      );
-    }
-  };
-
   const addFavorite = (id: string) => {
     setIsSettingFavorites(true);
-    setFavorites(f => {
-      const newFavorites = {...f, [id]: true};
-      setLocalStorageValues(newFavorites);
-      return newFavorites;
-    });
+    setFavorites(f => ({...f, [id]: true}));
     setTimeout(() => setIsSettingFavorites(false), 500);
   };
 
   const removeFavorite = (id: string) => {
     setIsSettingFavorites(true);
     setFavorites(f => {
-      const filteredFavorites = Object.entries(f).reduce((acc, [itemID]) => {
+      return Object.entries(f).reduce((acc, [itemID]) => {
         if (itemID !== id) {
           acc[itemID] = true;
         }
         return acc;
       }, {} as Favorites);
-      setLocalStorageValues(filteredFavorites);
-      return filteredFavorites;
     });
-    setTimeout(() => setIsSettingFavorites(false), 500);
+    setIsSettingFavorites(false);
   };
 
   const toggleFavorite = (id: string) => {
--- src/recoil/atoms.tsx
+++ src/recoil/atoms.tsx
@@ -1,9 +1,38 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import {atom} from 'recoil';
 
 // Array of map_id[]
 export type Favorites = Record<string, boolean>;
 
+export const FAVORITES_LOCAL_STORAGE_KEY = '@favorites';
+
 export const favoritesAtom = atom<Favorites>({
   key: 'favorites',
   default: {},
+  effects_UNSTABLE: [
+    ({onSet}) => {
+      onSet(async favorites => {
+        try {
+          const favoritesToSave = Object.entries(favorites).reduce(
+            (acc, [map_id, isFavorited]) => {
+              if (isFavorited) {
+                acc[map_id] = true;
+              }
+              return acc;
+            },
+            {} as Favorites,
+          );
+          await AsyncStorage.setItem(
+            FAVORITES_LOCAL_STORAGE_KEY,
+            JSON.stringify(favoritesToSave),
+          );
+        } catch (e) {
+          console.error(
+            `Error setting LocalStorage value ${FAVORITES_LOCAL_STORAGE_KEY}.`,
+          );
+        }
+      });
+    },
+  ],
 });
